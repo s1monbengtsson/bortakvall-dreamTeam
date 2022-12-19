f3be0354c6d0b4ad5e4adc4134aef1f9f92e7413
Jonas Nilsson <nilssoncjonas@gmail.com>
2022-12-19 16:03:14 +0100

    readded function to show info when product is clicked

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -101,6 +101,22 @@ document.querySelector('main')?.addEventListener('click', e => {
  ********************************************************************************
  * START
  */
+// start info-section
+const renderInfo = (x) => {
+    const temp = products.data[0] // changes to what user clicked on, dataset.Setid?
+    document.getElementById('#info-section')!.innerHTML = `    
+    <div class="info-section-l">
+        <img src="https://www.bortakvall.se/${temp.image.large}" alt="${temp.name}" class="mt-3 info-img">
+        <p id="product-name" class="mt-3">${temp.name}<span id="price">${temp.name}<span>kr</span></span></p>
+        <button class="btn btn-warning m-2" value="${temp.id}">Lägg till i varukorg</button>
+    </div>
+      <div class="mt-3 info-section-r"><h3 class="p-4">Beskrivning</h3>${temp.description}
+    </div>
+    `
+  }
+  // end info-section
+
+
 
 
 getProducts()
