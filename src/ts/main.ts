814318107bca81c042c881defff3578e15039598
Kristopher Gray <kristopher.gray99@gmail.com>
2022-12-19 14:25:45 +0100

    data-product-id added to product

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,30 +4,63 @@ import '../css/style.css'
 import { fetchProducts } from "./api"
 import { IData, IProduct } from "./interface"
 
-let products: IData[] = []
+let products: IData
 // let products: IProduct[] = []
+
 const getProducts = async () => {
   products = await fetchProducts()
+  console.log(products)
   renderProducts()
+  
 }
 
 const renderProducts = () => {
-  console.log(products)
-  console.log(products.status)
-  console.log(products.data)
-document.querySelector('.product-main')!.innerHTML = products.data.map( prod => `
-<div class="product-cards row col-3 my-5 bg-white>
-    <div class="product-wrap">
-        <img src="https://www.bortakvall.se${prod.images.thumbnail}" alt="${prod.name}" class="card-img-top card-img">
-        <div class="card-body">
-            <p id="product-name" class="card-title text-dark">${prod.name}</p>
-            <p id="product-price" class="card-text text-dark">${prod.price} kr</p>
-            <button type="" class="btn btn-warning py-2" id="${prod.id}" value="${prod.id}">Lägg till i<br> varukorg</button>
-        </div>
-    </div>
-</div>
 
- `).join('')
+    document.querySelector('.product-main')!.innerHTML = products.data
+        .map( prod => `
+            <div class="product-cards row col-3 my-5 bg-white>
+                <div class="product-wrap"  data-product-id="${prod.id}">
+                    <img src="https://www.bortakvall.se${prod.images.thumbnail}" alt="${prod.name}" class="card-img-top card-img product-wrap-child" data-product-id="${prod.id}">
+
+                    <div class="card-body product-wrap-child" data-product-id="${prod.id}">
+                        <p id="product-name" class="card-title text-dark product-wrap-child" data-product-id="${prod.id}">${prod.name}</p>
+
+                        <p id="product-price" class="card-text text-dark product-wrap-child" data-product-id="${prod.id}">${prod.price} kr</p>
+                        
+                        <button class="btn btn-warning py-2 product-wrap-child" data-product-id="${prod.id}">Lägg till i<br> varukorg</button>
+                    </div>
+                </div>
+            </div>
+        `)
+        .join('')
 }
 
 getProducts()
+
+
+
+// Click event on each product
+document.querySelector('main')?.addEventListener('click', e => {
+    const target = e.target as HTMLElement
+
+    // console.log(target.className)
+
+    if (target.className.includes('product-wrap' || 'product-wrap-child')) {
+
+        if (target.tagName === 'BUTTON') {
+            console.log('added to cart')
+            // addToCart()
+
+            const productId = Number(target.dataset.productId)
+            console.log('product id:', productId)
+        }
+        else {
+            console.log('viewing product')
+            // showInfo()
+
+            const productId = Number(target.dataset.productId)
+            console.log('product id:', productId)
+        }
+    }
+})
+
