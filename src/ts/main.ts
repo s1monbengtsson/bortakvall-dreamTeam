d9574dbffbc327f4b179b66ce386707935be35c6
Jonas Nilsson <nilssoncjonas@gmail.com>
2022-12-19 14:56:32 +0100

    Resolved merge conflict

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,47 +2,38 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../css/style.css'
 
 import { fetchProducts } from "./api"
-import { IData } from "./interface"
+import { IData, IProduct } from "./interface"
 
 let products: IData
 // let products: IProduct[] = []
+
 const getProducts = async () => {
   products = await fetchProducts()
   console.log(products)
   renderProducts()
-  renderInfo() // temp.  info-section, should only run when user clicked on product div
   
 }
 
 const renderProducts = () => {
-  console.log(products)
-  console.log(products.status)
-  console.log(products.data)  
-document.querySelector('.product-main')!.innerHTML = products.data.map( prod => `
-<div class="product-wrap">
-<img src="https://www.bortakvall.se${prod.images.thumbnail}" alt="${prod.name}">
-  <p id="product-name">${prod.name}<span id="price">${prod.price}<span>kr</span></span></p>
-  <button type="" id="${prod.id}">Lägg till i<br> varukorg</button>
-</div>
- `).join('')
 
+    document.querySelector('.product-main')!.innerHTML = products.data
+        .map( prod => `
+            <div class="product-cards row col-3 my-5 bg-white>
+                <div class="product-wrap"  data-product-id="${prod.id}">
+                    <img src="https://www.bortakvall.se${prod.images.thumbnail}" alt="${prod.name}" class="card-img-top card-img product-wrap-child" data-product-id="${prod.id}">
+                    <div class="card-body product-wrap-child" data-product-id="${prod.id}">
+                        <p id="product-name" class="card-title text-dark product-wrap-child" data-product-id="${prod.id}">${prod.name}</p>
+                        <p id="product-price" class="card-text text-dark product-wrap-child" data-product-id="${prod.id}">${prod.price} kr</p>
+                        
+                        <button class="btn btn-warning py-2 product-wrap-child" data-product-id="${prod.id}">Lägg till i<br> varukorg</button>
+                    </div>
+                </div>
+            </div>
+        `)
+        .join('')
 }
-// start info-section
-const renderInfo = (x) => {
-    const temp = products.data[0] // changes to what user clicked on, dataset.Setid?
-    document.getElementById('#info-section')!.innerHTML = `    
-    <div class="info-section-l">
-        <img src="https://www.bortakvall.se/${temp.image.large}" alt="${temp.name}" class="mt-3 info-img">
-        <p id="product-name" class="mt-3">${temp.name}<span id="price">${temp.name}<span>kr</span></span></p>
-        <button class="btn btn-warning m-2" value="${temp.id}">Lägg till i varukorg</button>
-    </div>
-      <div class="mt-3 info-section-r"><h3 class="p-4">Beskrivning</h3>${temp.description}
-    </div>
-    `
-}
-// end info-section
+
 getProducts()
-
 
 
 
@@ -71,3 +62,17 @@ document.querySelector('main')?.addEventListener('click', e => {
     }
 })
 
+// start info-section
+const renderInfo = (x) => {
+  const temp = products.data[0] // changes to what user clicked on, dataset.Setid?
+  document.getElementById('#info-section')!.innerHTML = `    
+  <div class="info-section-l">
+      <img src="https://www.bortakvall.se/${temp.image.large}" alt="${temp.name}" class="mt-3 info-img">
+      <p id="product-name" class="mt-3">${temp.name}<span id="price">${temp.name}<span>kr</span></span></p>
+      <button class="btn btn-warning m-2" value="${temp.id}">Lägg till i varukorg</button>
+  </div>
+    <div class="mt-3 info-section-r"><h3 class="p-4">Beskrivning</h3>${temp.description}
+  </div>
+  `
+}
+// end info-section
