c03819e13a56c3d223c46e2faa48a26af584a249
Jonas Nilsson <nilssoncjonas@gmail.com>
2022-12-19 19:38:19 +0100

    Added funtionallity for close info section

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -32,7 +32,6 @@ const getProducts = async () => {
     // console.log(products)
     renderProducts()
   products = await fetchProducts()
-  console.log(products)
   renderProducts()
   
 }
@@ -61,7 +60,7 @@ const findClickedProduct = async (clickedId: number) => {
 
     const products = await fetchProducts()
     const foundProduct: IProduct = products.data.find(prod => clickedId === prod.id) as IProduct
-    console.log('foundProduct:', foundProduct)
+    // console.log('foundProduct:', foundProduct)
     renderInfo(foundProduct)
     return foundProduct
 }
@@ -105,19 +104,28 @@ document.querySelector('main')?.addEventListener('click', e => {
 const renderInfo = (productInfo: IProduct) => {
     document.querySelector('.info-background')!.classList.remove('d-none')
     document.querySelector('.info-background')!.classList.add('show-info')
-    console.log(productInfo)
     document.querySelector('#info-section')!.innerHTML = `    
     <div class="info-section-l">
         <img src="https://www.bortakvall.se/${productInfo.images.large}" alt="${productInfo.name}" class="mt-3 info-img">
         <p class="info-name" class="mt-3">${productInfo.name}<span class="info-price">${productInfo.price}<span>kr</span></span></p>
-        <button class="btn btn-warning m-2 p-2" value="${productInfo.id}">Lägg till i varukorg</button>
+        <button class="btn btn-warning m-2 p-2" data-prod-id="${productInfo.id}">Lägg till i varukorg</button>
     </div>
       <div class="mt-3 info-section-r"><h3 class="p-4">Beskrivning</h3>${productInfo.description}
     </div>
     `
   }
+document.querySelector('.info-background')!.addEventListener('click', e => {
+    const target = e.target as HTMLElement
+    if (target.tagName === 'BUTTON') {
+        console.log(Number(target.dataset.prodId))
+        console.log('added to cart')
+        // findClickedProduct(clickedId)
+    }
+    else {
+        document.querySelector('.info-background')!.classList.add('d-none')
+    }
+})
 
-  
   // end info-section
 
 
