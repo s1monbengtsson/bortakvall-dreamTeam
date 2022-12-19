bd7e45d56e7ad9c5933412841a4032bbed82fb2b
Jonas Nilsson <nilssoncjonas@gmail.com>
2022-12-19 09:59:18 +0100

    added main ts, changed pathways

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 
+import { fetchProducts } from "./ts/api"
+import { IData, IProduct } from "./ts/interface"
+
+let products: IData[] = []
+// let products: IProduct[] = []
+const getProducts = async () => {
+  products = await fetchProducts()
+  renderProducts()
+}
+
+const renderProducts = () => {
+  console.log(products)
+  console.log(products.status)
+  console.log(products.data)
+document.querySelector('.product-main')!.innerHTML = products.data.map( prod => `
+<div class="product-wrap">
+<img src="https://www.bortakvall.se${prod.images.thumbnail}" alt="${prod.name}">
+  <p id="product-name">${prod.name}<span id="price">${prod.price}<span>kr</span></span></p>
+  <button type="" id="${prod.id}">Lägg till i<br> varukorg</button>
+</div>
+ `).join('')
+}
+
+getProducts()
