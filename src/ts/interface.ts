b11802a6b9386d480323888f4011dedc452eef15
Simon Bengtsson <simonbengan8@gmail.com>
2022-12-21 21:43:37 +0100

    added interface for customer data

--- src/ts/interface.ts
+++ src/ts/interface.ts
@@ -34,6 +34,16 @@ export interface IOrder {
     }[],
 }
 
+export interface ICustomerInfo {
+    customer_first_name: string,
+    customer_last_name: string,
+    customer_address: string,
+    customer_postcode: number,
+    customer_city: string,
+    customer_phone?: string,
+    customer_email: string
+}
+
 
 
 /* 
