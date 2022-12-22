b278fd3a5e15e5494a1e0f5d042c55d3eacf6b81
Kristopher Gray <kristopher.gray99@gmail.com>
2022-12-22 22:20:28 +0100

    restored changes just before merge from cart

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,7 @@
 /**
  ********************************************************************************
- * IMPORTS
- */
+* IMPORTS
+*/
 
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -14,8 +14,8 @@ import { IData, IProduct, IOrder } from "./interface"
 
 /**
  ********************************************************************************
- * VARIABLES
- */
+* VARIABLES
+*/
 
 let products: IData
 // let products: IProduct[] = []
@@ -88,8 +88,8 @@ const countTotalPrice = () => {
 
 /**
  ********************************************************************************
- * FUNCTIONS
- */
+* FUNCTIONS
+*/
 
 
 // Allt denna funktion ska göra är att hitta produkten man clickar på
@@ -201,8 +201,8 @@ const renderProducts = (): void => {
 
 /**
  ********************************************************************************
- * EVENT LISTENERS
- */
+* EVENT LISTENERS
+*/
 
 
 
@@ -281,8 +281,8 @@ document.querySelector('#clear-cart-btn')?.addEventListener('click', async () =>
 
 /**
  ********************************************************************************
- * START
- */
+* START
+*/
 
 
 // start info-section
@@ -295,8 +295,8 @@ const renderInfo = (productInfo: IProduct) => {
         <p class="info-name" class="mt-3">${productInfo.name}<span class="info-price">${productInfo.price}<span>kr</span></span></p>
         <button class="btn btn-warning m-2 p-2" data-prod-id="${productInfo.id}" style="font-weight: bold;">Lägg till i varukorg</button>
     </div>
-      <div class="info-section-r"><h3>Beskrivning</h3>${productInfo.description}
-      <p class="info-close"><i class="bi bi-x-lg"></i></p>
+    <div class="info-section-r"><h3>Beskrivning</h3>${productInfo.description}
+    <p class="info-close"><i class="bi bi-x-lg"></i></p>
     </div>
     `
 }
