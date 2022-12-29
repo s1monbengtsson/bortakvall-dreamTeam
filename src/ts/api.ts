f949c2688c0766a9242e241d3d6683afc1116f9b
Simon Bengtsson <dev.simon.bengtsson@gmail.com>
2022-12-29 18:35:26 +0100

    removed code for success handling and added to main.ts instead

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,4 +1,4 @@
-import { IData, IOrder } from "./interface";
+import { IData, IOrder, IPostData, } from "./interface";
 
 
 /* Fetch products from api */
@@ -29,36 +29,10 @@ export const createNewOrder = async (order: IOrder) => {
         body: JSON.stringify(order),
     })
 
-    const orderInfo =  await res.json() 
-
-
   if (!res.ok) {
 		throw new Error(`${res.status} ${res.statusText}`)
-	} else {
-        document.querySelector('.checkout-wrap')!.classList.add('d-none')
-        document.querySelector('.back-button')!.classList.add('d-none')
-        document.querySelector('.order-confirmation')!.innerHTML += `
-            <h2> Tack för din beställning!</h2>
-            <div class="border rounded w-75 py-5 my-5 mx-auto">
-            <h3 class=""> Orderbekräftelse</h3>
-            <p>Ordernummer: <strong>${orderInfo.data.id}</strong></p>
-            <p>Beställningsdatum: <strong>${orderInfo.data.order_date}</strong></p>
-            <p>Betalt: <strong>${orderInfo.data.order_total} kr</strong></p>
-            </div>
-            <p class="mt-5 text-muted">Du kan nu stänga sidan!</p>
-            <button class="btn btn-dark close">Stäng</button>
-        `
-        document.querySelector('.close')!.addEventListener('click', () => {
-            window.location.reload()
-            localStorage.removeItem('Total price')
-            localStorage.removeItem('Shopping cart')
-        })
-        
+
     }
-
-
-    
-    
-
+    return await res.json() as IPostData
 
 }
