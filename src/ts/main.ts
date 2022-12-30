941d1c70c86f37fa840d5cb3c95959fd45d6e88f
Jonas Nilsson <nilssoncjonas@gmail.com>
2022-12-30 11:58:28 +0100

    more fixes for svg and klick event

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -188,10 +188,10 @@ const renderProducts = (): void => {
                 <div class="card-body">
                     <p id="product-name" class="card-title product-wrap-child" data-product-id="${prod.id}">${prod.name}</p>
                     <p id="product-price" class="card-text text-dark">${prod.price} kr</p>
-                    <p class="info-icon-wrap">     
+                    <p class="info-icon-wrap product-wrap-child">     
                         <svg xmlns="http://www.w3.org/2000/svg" class="product-wrap-child bi-info-square" id="info-icon" data-product-id="${prod.id}" width="35" height="35" fill="currentColor" class="bi bi-info-square" viewBox="0 0 16 16">
-                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                        <path class="product-wrap-child" d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                        <path class="product-wrap-child" d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                         </svg>
                     </p>
                     <button class="product-wrap-child product-btn ${(prod.stock_status === 'outofstock') ? 'product-btn-outofstock' : ''}" data-product-id="${prod.id}" ${(prod.stock_status === 'outofstock') ? 'disabled' : ''}>
@@ -244,6 +244,9 @@ const increaseQty = (prod: IProduct) => {
 // Click event on each product
 dqs('main').addEventListener('click', async e => {
     const target = e.target as HTMLElement
+    console.log(target.tagName)
+    console.log(target)
+    console.log(target.className)
     const clickedId = Number(target.dataset.productId)
     const clickedProduct = await findClickedProduct(clickedId)
     
@@ -262,10 +265,12 @@ dqs('main').addEventListener('click', async e => {
         },950)
     }
     // Om man klickar någon annan stans på produkten. (info)
-    else if (target.tagName === 'svg') {
+    else if (target.tagName === 'svg','IMG','p') {
+
         renderInfo(clickedProduct)
         document.body.style.overflow = 'hidden';
     }
+    
 })
 
 // View cart
@@ -337,7 +342,7 @@ dqs('.info-background').addEventListener('click', async e => {
             dqs('#cart-wrap').classList.remove('shake')
         },950)
     }
-    else if (target.tagName === 'svg' || target.tagName ==='path') {
+    else if (target.tagName === 'svg','path') {
         hide('.info-background')
         document.body.style.removeProperty('overflow');
     }
