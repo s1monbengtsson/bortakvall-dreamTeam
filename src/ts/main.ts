23f61cb0a02b3622032de5d5923e8e946bbbc7ed
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-01-03 15:55:44 +0100

    adjusted duplicate class

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -96,7 +96,7 @@ const renderCartItems = () => {
                     <p class="card-text-cart text-dark cart-item-price">${item.price} kr/st  </p>
                     <p class="card-text-cart text-dark" id="item-price-${item.id}">${item.price * item.qty} kr</p>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="bi-trash cart-remove-item" data-product-id="${item.id}" width="40" height="40" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" class="bi-x-circle cart-remove-item" data-product-id="${item.id}" width="40" height="40" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
